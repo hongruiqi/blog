6855ff1571126d02ef6304ac6cddd1b616f98c7b
hongruiqi <hongruiqi@gmail.com>
2016-05-11 14:19:49 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
-hugo -t crisp
-git add .
-git commit -a -m "commit"
-git pull
-git push
-#git subtree add --prefix=public --squash git@github.com:hongruiqi/hongruiqi.github.io.git master
-git subtree push --prefix=public --squash git@github.com:hongruiqi/hongruiqi.github.io.git master
+git subtree push --prefix=public . gh-pages
+git subtree push --prefix=content . contents
+git push --all
